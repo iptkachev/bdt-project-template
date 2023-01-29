cf62a228eddf515745b3ece24d188243a5625736
Ilya Tkachev <skaatt@mail.ru>
2023-01-29 20:01:54 +0100

    added template for init work

---
+++ knn.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+
+class KNN:
+    """
+    K-neariest-neighbor classifier using L1 loss
+    """
+    def __init__(self, k=1):
+        self.k = k
+
+    def fit(self, X):
+        self.train_X = X
+
+    def kneighbors(self, X, num_loops=0):
+        '''
+        Uses the KNN model to predict kneighbors in train for the data samples provided
+        
+        Arguments:
+        X, np array (num_samples, num_features) - samples to run
+           through the model
+        num_loops, int - which implementation to use
+
+        Returns:
+        kneighbors, np array of ints (num_samples x k) - predicted kneighbors indexes
+           for each sample
+        '''
+        if num_loops == 0:
+            dists = self.compute_distances_no_loops(X)
+        elif num_loops == 1:
+            dists = self.compute_distances_one_loop(X)
+        else:
+            dists = self.compute_distances_two_loops(X)
+
+        return self.find_kneighbors_by_dists(dists)
+
+    def compute_distances_two_loops(self, X):
+        '''
+        Computes L1 distance from every sample of X to every training sample
+        Uses simplest implementation with 2 Python loops
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        '''
+        num_train = self.train_X.shape[0]
+        num_test = X.shape[0]
+        dists = np.zeros((num_test, num_train), np.float32)
+        for i_test in range(num_test):
+            for i_train in range(num_train):
+                # TODO: Fill dists[i_test][i_train]
+                pass
+
+    def compute_distances_one_loop(self, X):
+        '''
+        Computes L1 distance from every sample of X to every training sample
+        Vectorizes some of the calculations, so only 1 loop is used
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        '''
+        num_train = self.train_X.shape[0]
+        num_test = X.shape[0]
+        dists = np.zeros((num_test, num_train), np.float32)
+        for i_test in range(num_test):
+            # TODO: Fill the whole row of dists[i_test]
+            # without additional loops or list comprehensions
+            pass
+
+    def compute_distances_no_loops(self, X):
+        '''
+        Computes L1 distance from every sample of X to every training sample
+        Fully vectorizes the calculations using numpy
+
+        Arguments:
+        X, np array (num_test_samples, num_features) - samples to run
+        
+        Returns:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+        '''
+        num_train = self.train_X.shape[0]
+        num_test = X.shape[0]
+        # Using float32 to to save memory - the default is float64
+        dists = np.zeros((num_test, num_train), np.float32)
+        # TODO: Implement computing all distances with no loops!
+        pass
+
+    def find_kneighbors_by_dists(self, dists):
+        '''
+        Returns model predictions for binary classification case
+        
+        Arguments:
+        dists, np array (num_test_samples, num_train_samples) - array
+           with distances between each test and each train sample
+
+        Returns:
+        kneighbors, np array of ints (num_samples x k) - predicted kneighbors indexes
+           for each sample
+        '''
+        pass
